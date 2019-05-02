cc39eb8eca23ee0a24fe6b803cc294137001844e
michkrzeminski <michkrzeminski@gmail.com>
2019-05-02 06:37:41 +0200

    new initial

---
+++ app/src/main/java/pl/net/gwynder/central/client/CentralApplication.kt
@@ -0,0 +1,11 @@
+package pl.net.gwynder.central.client
+
+import android.app.Application
+
+class CentralApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/pl/net/gwynder/central/client/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package pl.net.gwynder.central.client.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.net.gwynder.central.client.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
